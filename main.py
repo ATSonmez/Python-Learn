e98e7f62ebcec2c0231210cdbc9982a44980955a
ATSonmez <168234885+ATSonmez@users.noreply.github.com>
2025-06-25 20:19:04 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,49 @@
+#Strings
 first_name = "Ahmed"
+food = "pizza"
+email = "Bro123@fake.com"
 
-print(first_name)
-print("hello")
-print("h")
+print(f"Hello {first_name}")
+print(f"You like {food}")
+print(f"Your email is {email}")
+
+#Integers
+age = 25
+quantity = 3
+num_of_students = 30
+
+print(f"You are {age} years old")
+print(f"You are buying {quantity} items")
+print(f"Your class had {num_of_students} students")
+
+#Floats
+price = 10.99
+gpa = 3.2
+distance = 5.5
+
+print(f"The price is {price} dollars")
+print(f"Your gpa is: {gpa}")
+print(f"You ran {distance} miles")
+
+#Booleans
+is_student = True
+for_sale = True
+is_online = True
+
+print(f"Are you a student? {is_student}")
+
+if is_student:
+    print ("You are a student")
+else:
+    print ("You are not a student")
+
+if for_sale:
+    print ("This item is for sale")
+else:
+    print ("This item is not available for sale")
+
+if is_online:
+    print ("You are online")
+else: 
+    print ("You are offline")
+
