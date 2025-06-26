1bcba55c66baa881c5d0fd436d11516ddaef0523
ATSonmez <168234885+ATSonmez@users.noreply.github.com>
2025-06-25 20:43:33 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-length = float(input("Enter the length: "))
-width = float(input("Enter the width: "))
-area = length * width
+item = input("What item would you like to buy?: ")
+price = float(input("What is the price?: "))
+quantity = int(input("How many would you like?: "))
+total = price * quantity
 
-print(f"The area is: {area} cm²") 
+print(f"You have bough {quantity} x {item}/s")
+print(f"Total cost: ${total}")
