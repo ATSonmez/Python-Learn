5036278a1839c44123c3fe8f551641570dc69c83
ATSonmez <168234885+ATSonmez@users.noreply.github.com>
2025-06-26 22:43:01 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-food = input("Enter a food you like (q to quit): ")
+num = int(input("Enter a # between 1 and 10: "))
 
-while not food == "q":
-    print(f"You like {food}")
-    food = input("Enter another food you like (q to quit): ")
+while num < 1 or num > 10:
+    print(f"{num} is not valid")
+    num = int(input("Enter a # between 1 and 10: "))
 
-print("bye")
+print(f"Your number is {num}")
