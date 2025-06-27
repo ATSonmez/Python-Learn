45260df43e9bc5a7c9d1886441623cbac44a08c1
ATSonmez <168234885+ATSonmez@users.noreply.github.com>
2025-06-26 20:34:46 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-#Left at calculator program of Python Full Course 2024 BroCode
+operator = input("Enter an operator (+ - * /): ")
+num1 = float(input("Enter the 1st number: "))
+num2 = float(input("Enter the 2nd number: "))
+
+if operator == "+":
+    result = num1 + num2
+    print(round(result))
+elif operator == "-":
+    result = num1 - num2
+    print(round(result))
+elif operator == "*":
+    result = num1 * num2
+    print(round(result))
+elif operator == "/":
+    result = num1 / num2
+    print(round(result))
+else:
+    print(f"{operator} is not a valid operator")
