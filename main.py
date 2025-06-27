2a4e5c713a9705f8aaf07beebecde1e176d8bd91
ATSonmez <168234885+ATSonmez@users.noreply.github.com>
2025-06-26 21:12:02 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-temp = 0
-is_sunny = True
+num = 5
+a = 6
+b = 7
+age = 13
+temperature = 19
+user_role = "admin"
 
-if temp >= 28 and is_sunny:
-    print("It is Hot outside")
-    print("It is Sunny")
-elif temp <= 0 and is_sunny:
-    print("It is Cold outside")
-    print("It is Sunny")
-elif temp < 28 and temp > 0 and is_sunny:
-    print("It is Warm outside")
-    print("It is Sunny")
+#print("Positive" if num > 0 else "Negative")
+#result = "Even" if num % 2 == 0 else "Odd"
+#max_num = a if a > b else b
+#min_num = a if a < b else b
+#status = "Adult" if age >= 18 else "Child"
+#weather = "Hot" if temperature > 20 else "Cold"
+access_level = "Full Access" if user_role == "admin" else "Limited Access"
+
+print(access_level)
+
+
+
