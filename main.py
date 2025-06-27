03a6c42855b4157bd334b0b0ef86c8d6f1694059
ATSonmez <168234885+ATSonmez@users.noreply.github.com>
2025-06-26 22:24:19 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-username = input("Enter a username: ")
+credit_number = "1234-5678-9012-3456"
 
-if len(username) > 12:
-    print("Your username can't be more than 12 characters")
-elif not username.find(" ") == -1:
-    print("Your username can't containt spaces")
-elif not username.isalpha():
-    print("Your username can't contain numbers")
-else:
-    print(f"Welcome {username}")
+#print(credit_number[4])
+#print(credit_number[:4])
+#print(credit_number[5:9])
+#print(credit_number[5:])
+#print(credit_number[-1])
+#print(credit_number[::3])
+#last_digits = credit_number[-4:]
 
+credit_number = credit_number[::-1]
+
+print(credit_number)
 
 
