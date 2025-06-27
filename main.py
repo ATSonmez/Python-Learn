c723431492eaf07dfe4417bc8f76922aebe1b80e
ATSonmez <168234885+ATSonmez@users.noreply.github.com>
2025-06-26 22:41:11 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-age = int(input("Enter your age: "))
+food = input("Enter a food you like (q to quit): ")
 
-while age < 0:
-    print("Age can't be negative")
-    age = int(input("Enter your age: "))
+while not food == "q":
+    print(f"You like {food}")
+    food = input("Enter another food you like (q to quit): ")
 
-print(f"You are {age} years old.")
+print("bye")
