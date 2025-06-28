e893c7dec1b61c0d3242ccc5ef712e549bb7af3e
ATSonmez <168234885+ATSonmez@users.noreply.github.com>
2025-06-27 20:00:55 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-num = int(input("Enter a # between 1 and 10: "))
+principle = 0
+rate = 0
+time = 0
 
-while num < 1 or num > 10:
-    print(f"{num} is not valid")
-    num = int(input("Enter a # between 1 and 10: "))
+while principle <= 0:
+    principle = float(input("Enter the principle amount: "))
+    if principle <= 0:
+        print("Principle can't be less than or equal to zero")
 
-print(f"Your number is {num}")
+while rate <= 0:
+    rate = float(input("Enter the interest rate : "))
+    if rate <= 0:
+        print("Interest rate can't be less than or equal to zero")
+
+while time <= 0:
+    time = int(input("Enter the time in years: "))
+    if time <= 0:
+        print("Time can't be less than or equal to zero")
+
+total = principle  * pow((1+rate/100), time)
+print(f"Balance after {time} year/s: ${total:.2f}")
+        
+
