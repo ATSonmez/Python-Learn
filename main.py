dacb42c824f6bb308ee04b6a6e12f775c99a991d
ATSonmez <168234885+ATSonmez@users.noreply.github.com>
2025-06-28 17:52:43 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,12 @@
-principle = 0
-rate = 0
-time = 0
+import time
 
-while principle <= 0:
-    principle = float(input("Enter the principle amount: "))
-    if principle <= 0:
-        print("Principle can't be less than or equal to zero")
+my_time = int(input("Enter the time in seconds: "))
 
-while rate <= 0:
-    rate = float(input("Enter the interest rate : "))
-    if rate <= 0:
-        print("Interest rate can't be less than or equal to zero")
+for x in range(my_time, 0, -1):
+    seconds = x % 60
+    minutes = int(x / 60) % 60
+    hours = int(x / 3600)
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
+    time.sleep(1)
 
-while time <= 0:
-    time = int(input("Enter the time in years: "))
-    if time <= 0:
-        print("Time can't be less than or equal to zero")
-
-total = principle  * pow((1+rate/100), time)
-print(f"Balance after {time} year/s: ${total:.2f}")
-        
-
+print("Time's Up!")
