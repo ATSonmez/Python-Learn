2c72f43442ce64f04552ff599e19c098d3ebe038
ATSonmez <168234885+ATSonmez@users.noreply.github.com>
2025-06-28 19:29:44 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
+num_pad = ((1 ,2 ,3), 
+           (4, 5, 6), 
+           (7, 8, 9), 
+           ("*", 0, "#"))
 
-groceries = [("apple", "orange", "banana", "coconut"), 
-             ("celery", "carrots", "potatoes") , 
-             ("chicken", "fish", "turkey")]
-
-
-for collection in groceries:
-    for food in collection:
-        print(food, end = " ")
+for row in num_pad:
+    for num in row:
+        print(num, end = " ")
     print()
