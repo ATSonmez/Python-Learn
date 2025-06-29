db142a11bc78129380fe7c7a055e0b72affde2df
ATSonmez <168234885+ATSonmez@users.noreply.github.com>
2025-06-28 19:12:27 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,10 @@
-foods = [] 
-prices = []
-total = 0
 
-while True:
-    food = input("Enter a food to buy (q to quit): ")
-    if food.lower() == "q":
-        break
-    else:
-        price = float(input(f"Enter the price for {food}: $"))
-        foods.append(food)
-        prices.append(price)
+groceries = [("apple", "orange", "banana", "coconut"), 
+             ("celery", "carrots", "potatoes") , 
+             ("chicken", "fish", "turkey")]
 
-print("----- Your Cart -----")
 
-for food in foods:
-    print(food, end = " ")
-
-for price in prices:
-    total += price
-
-print(f"Your total is : ${total}")
+for collection in groceries:
+    for food in collection:
+        print(food, end = " ")
+    print()
