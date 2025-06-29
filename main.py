90dbbc6d7fc76101a9abbe5e29ddb2dccc0103e5
ATSonmez <168234885+ATSonmez@users.noreply.github.com>
2025-06-28 19:39:34 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
-num_pad = ((1 ,2 ,3), 
-           (4, 5, 6), 
-           (7, 8, 9), 
-           ("*", 0, "#"))
+questions = ("How many elements are in the periodic table?", 
+             "which animal lays the ;argest eggs", 
+             "Abundat gas", 
+             "Bones body", 
+             "planet")
 
-for row in num_pad:
-    for num in row:
-        print(num, end = " ")
-    print()
+
+options = (("A", "B", "C",  "D"), 
+           ("A", "B", "C",  "D"), 
+           ("A", "B", "C1",  "D"), 
+           ("A", "B", "C",  "D"), 
+           ("A", "B", "C",  "D"))
+
+answers = ("C", "D", "A", "A", "B")
+guesses = []
+score = 0 
+question_num = 0
+
+for question in questions:
+    print("---------------")
+    print(question)
+    for option in options[question_num]:
+        print(option)
+    guess = input("Enter (A, B, C, D): ").upper()
+    guesses.append(guess)
+    if guess == answers[question_num]:
+        score += 1
+        print("Correct!")
+    else:
+        print("Incorrect")
+        print(f"{answers[question_num]} is the correct answer")
+        
+    question_num += 1
