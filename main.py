001f2836e21ea30948dc56d61d611398f360a72c
ATSonmez <168234885+ATSonmez@users.noreply.github.com>
2025-06-28 18:37:25 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-import time
+fruits = ("apple", "orange", "banana", "coconut", "coconut")
+print(fruits.count("coonut"))
 
-my_time = int(input("Enter the time in seconds: "))
+#print(dir(fruits))
+#print(help(fruits))
+#print("apple" in fruits=
 
-for x in range(my_time, 0, -1):
-    seconds = x % 60
-    minutes = int(x / 60) % 60
-    hours = int(x / 3600)
-    print(f"{hours:02}:{minutes:02}:{seconds:02}")
-    time.sleep(1)
+#fruits[1] = "pineapple"
+#fruits.append("pineapple")
+#fruits.remove("apple")
+#fruits.insert(0, "pineapple")
+#fruits.sort()
+#fruits.reverse()
+#fruits.clear()
+#print(fruits.index("apple"))
+#print(fruits.count("pine"))
+#print(len(fruits))
+#print("pineapple" in fruits)
+#fruits.pop()
 
-print("Time's Up!")
+
+print(fruits)
