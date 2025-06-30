8ffe617f0f270975cdb74d142ee0e88e8b49e257
ATSonmez <168234885+ATSonmez@users.noreply.github.com>
2025-06-29 19:36:46 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
-import random 
+import random
 
-low = 1
-high = 100
-options = ("rock", "paper", "scissors")
-cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+lowest_num = 1
+highest_num = 100
+answer = random.randint(lowest_num, highest_num)
+guesses = 0
+is_running = True
 
-#number = random.randint(low, high)
-#number = random.random()
-#options = random.choice(options)
-random.shuffle(cards)
+print("Python Number Guessing Game")
+print(f"Select a number between {lowest_num} and {highest_num}")
 
-print(cards)
+while is_running:
+    
+    guess = input("Enter your guess: ")
+    
+    if guess.isdigit():
+        guess = int(guess)
+        guesses += 1
+        
+        if guess < lowest_num or guess > highest_num:
+            print("That number is out of range")
+            print(f"Please select a number between {lowest_num} and {highest_num}")
+        elif guess < answer:
+            print("Too low! Try again!")
+        elif guess > answer:
+            print("Too high! Try again!")
+        else:
+            print(f"Correct! The answer was {answer}")
+            print(f"Number of guesses: {guesses}")
+            is_running = false
+    else:
+        print("Invalid guess")
+        print(f"Please select a number between {lowest_num} and {highest_num}")
+
+
