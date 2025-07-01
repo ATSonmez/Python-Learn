61298772caaeaf7e47343a05bb01b53a94469922
ATSonmez <168234885+ATSonmez@users.noreply.github.com>
2025-06-30 18:08:29 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,57 +1,10 @@
-import random
+def print_address(**kwargs):
+    for key, value in kwargs.items():
+        print(f"{key}: {value}")
 
-# ● ┌ ─ ┐ │ └ ┘
-
-dice_art = {
-    1: ("┌─────────┐", 
-        "│         │", 
-        "│    ●    │", 
-        "│         │", 
-        "└─────────┘"),
-    2: ("┌─────────┐", 
-        "│  ●      │", 
-        "│         │", 
-        "│      ●  │", 
-        "└─────────┘"),
-    3: ("┌─────────┐", 
-        "│  ●      │", 
-        "│    ●    │", 
-        "│      ●  │", 
-        "└─────────┘"),
-    4: ("┌─────────┐", 
-        "│  ●   ●  │", 
-        "│         │", 
-        "│  ●   ●  │", 
-        "└─────────┘"),
-    5: ("┌─────────┐", 
-        "│  ●   ●  │", 
-        "│    ●    │", 
-        "│  ●   ●  │", 
-        "└─────────┘"),
-    6: ("┌─────────┐", 
-        "│  ●   ●  │", 
-        "│  ●   ●  │", 
-        "│  ●   ●  │", 
-        "└─────────┘")
-    }
-
-dice = []
-total = 0
-num_of_dice = int(input("How many dice?: "))
-
-for die in range(num_of_dice):
-    dice.append(random.randint(1, 6))
-
-#for die in range(num_of_dice):
-    #for line in dice_art.get(dice[die]):
-       # print(line)
-
-for line in range(5):
-    for die in dice:
-        print(dice_art.get(die)[line], end= "")
-    print()
-
-for die in dice:
-    total += die
-print(f"total: {total}")
+print_address(street= "123 Fake St.", 
+              apt="100",
+              city= "Detroit", 
+              state= "MI", 
+              zip= "54321")
 
