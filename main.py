3bad363bf5a587b9992bbba5a8f150c42180c136
ATSonmez <168234885+ATSonmez@users.noreply.github.com>
2025-06-30 18:18:24 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-def print_address(**kwargs):
-    for key, value in kwargs.items():
-        print(f"{key}: {value}")
+def shipping_label(*args, **kwargs):
+    for arg in args:
+        print(arg, end= " ")
+    print()
+    if "apt" in kwargs:
+        print(f"{kwargs.get('street')} {kwargs.get('apt')}")
+    elif "pobox" in kwargs:
+        print(f"{kwargs.get('street')}")
+        print(f"{kwargs.get('pobox')}")
+    else:
+       print(f"{kwargs.get('street')}")
+    print(f"{kwargs.get('city')} {kwargs.get('state')} {kwargs.get('zip')}")
+      
 
-print_address(street= "123 Fake St.", 
-              apt="100",
-              city= "Detroit", 
-              state= "MI", 
-              zip= "54321")
+shipping_label("Dr.", "Spongebob", "SquarePants", "III",
+               street= "123 Fake St.", 
+               pobox= "PO box #100",
+               city= "Detroit", 
+               state= "MI", 
+               zip= "54321")
 
