65891ce21d1570be651f3e8129d4d8182724d9b4
ATSonmez <168234885+ATSonmez@users.noreply.github.com>
2025-07-03 16:14:42 -0700

    Update testing12.py

--- testing12.py
+++ testing12.py
@@ -1,3 +1,13 @@
-pi = 1123
+pi = 3.14159
 
-print(pi)
+def square(x):
+    return x ** 2
+
+def cube(x):
+    return x ** 3
+
+def circumference(radius):
+    return 2 * pi * radius
+
+def area(radius):
+    return pi * radius **2
