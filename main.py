d69c1577646aa5df19f04652a276af75e3c11cd8
ATSonmez <168234885+ATSonmez@users.noreply.github.com>
2025-07-03 17:34:51 -0700

    Update main.py

--- main.py
+++ main.py
@@ -21,28 +21,31 @@ def withdraw():
     else:
         return amount
     
+def main():
+    balance = 0
+    is_running = True
 
-balance = 0
-is_running = True
+    while is_running:
+        print("Banking Program")
+        print("1.Show Balance")
+        print("2.Deposit")
+        print("3.Withdraw")
+        print("4.Exit")
 
-while is_running:
-    print("Banking Program")
-    print("1.Show Balance")
-    print("2.Deposit")
-    print("3.Withdraw")
-    print("4.Exit")
+        choice = input("Enter your choice (1-4): ")
 
-    choice = input("Enter your choice (1-4): ")
+        if choice == '1':
+            show_balance()
+        elif choice == '2':
+            balance += deposit()
+        elif choice == '3':
+            balance -= withdraw()
+        elif choice == '4':
+            is_running = False
+        else:
+            print("That is not a valid choice")
 
-    if choice == '1':
-        show_balance()
-    elif choice == '2':
-        balance += deposit()
-    elif choice == '3':
-        balance -= withdraw()
-    elif choice == '4':
-        is_running = False
-    else:
-        print("That is not a valid choice")
+    print("Thank you! Have a nice day!")
 
-print("Thank you! Have a nice day!")
+if __name__ == '__main__':
+    main()
