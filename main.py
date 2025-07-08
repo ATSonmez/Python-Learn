69fa9df0f96dae76e3f7931eb46868e9f0dd65bb
ATSonmez <168234885+ATSonmez@users.noreply.github.com>
2025-07-07 17:21:31 -0700

    sd
    d

--- main.py
+++ main.py
@@ -1,33 +1,11 @@
-import random
-import string
+from car import Car
 
-chars = " " + string.punctuation + string.digits + string.ascii_letters
-chars = list(chars)
-key = chars.copy()
+car1 = Car("Mustang", 2024, "red", False)
+car2 = Car("Corvette", 2025, "blue", True)
+car3 = Car("Charger", 2026, "Yellow", True)
 
-random.shuffle(key)
 
-#print(f"chars: {chars}")
-#print(f"key: {key}")
+car3.drive()
+car3.stop()
+car3.describe()
 
-#Encrypt
-plain_text = input("Enter a message to encrypt: ")
-cipher_text = ""
-
-for letter in plain_text: 
-    index = chars.index(letter)
-    cipher_text += key[index]
-
-print(f"original message: {plain_text}")
-print(f"encrypted message: {cipher_text}")
-
-#Decrypt
-cipher_text = input("Enter a message to Decrypt: ")
-plain_text = ""
-
-for letter in cipher_text: 
-    index = key.index(letter)
-    plain_text += chars[index]
-
-print(f"encrypted message: {cipher_text}")
-print(f"original message: {plain_text}")
